f76c25e516270f42acdeb465d52aae87bcb21e20
FerlinGH <Grygorenko.d@ukr.net>
2016-09-19 12:49:23 +0300

    Delete MyException.java

--- Lesson4Home1/src/net/ukr/grygorenko_d/MyException.java
+++
@@ -1,8 +0,0 @@
-package net.ukr.grygorenko_d;
-
-public class MyException extends Exception {
-	@Override
-	public String getMessage() {
-		return "Error! Group is full, cannot add another student!";
-	}
-}
